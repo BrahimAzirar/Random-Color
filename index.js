8cb2ab1dd0bc54dc607fa40be429338a9ed7973b
Brahim_Azirar <90454287+BrahimAzirar@users.noreply.github.com>
2023-01-27 13:41:50 +0100

    I ended the Project

--- index.js
+++ index.js
@@ -14,14 +14,44 @@ function GetRandomColor() {
 function RGB_ToObject(element) {
     const color = element.style.background.match(/\d+/g);
 
-    return {r: color[0], g: color[1], b: color[2]};
+    return {r: parseInt(color[0]), g: parseInt(color[1]), b: parseInt(color[2])};
 }
+
+async function copyText(Element) {
+
+    const ELementColor = Element.textContent
+
+    try {
+        await navigator.clipboard.writeText(Element.textContent);
+
+        Element.textContent = "Copy";
+
+        setTimeout(() => {
+            Element.textContent = ELementColor;
+        }, 2000);
+    } catch (err) {
+        console.error('Failed to copy text: ', err);
+    }
+}
+
 
 window.onload = () => {
 
     Array.from(ColorCard).forEach(ele => {
         ele.firstElementChild.style.background = GetRandomColor();
-        console.log(rgbToHex(RGB_ToObject(ele.firstElementChild)));
         ele.lastElementChild.textContent = rgbToHex(RGB_ToObject(ele.firstElementChild));
     });
 }
+
+
+
+
+btn.onclick = () => {
+    window.location.reload();
+}
+
+Array.from(ColorCard).forEach(ele => {
+    ele.onclick = () => {
+        copyText(ele.lastElementChild);
+    }
+});
